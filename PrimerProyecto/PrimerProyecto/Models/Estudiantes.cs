39148a24f4a60cd863a2342f355c4b04fd3c1f63
DjMarte <alphat098@gmail.com>
2024-08-16 21:08:32 -0400

    Realicé modelo estudiantes

---
+++ PrimerProyecto/PrimerProyecto/Models/Estudiantes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerProyecto.Models;
+
+public class Estudiantes
+{
+    [Key]
+    public int EstudianteId { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public int Matricula { get; set; }
+
+    public string? Carrera { get; set; }
+
+}
