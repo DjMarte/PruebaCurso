b08bf89e9d7db592801ecd12ae23584729589d12
DjMarte <alphat098@gmail.com>
2024-08-20 21:45:56 -0400

    Agregué RegularExpressions y le cambié el tipo de dato a Matricula.

--- PrimerProyecto/PrimerProyecto/Models/Estudiantes.cs
+++ PrimerProyecto/PrimerProyecto/Models/Estudiantes.cs
@@ -7,10 +7,17 @@ public class Estudiantes
     [Key]
     public int EstudianteId { get; set; }
 
+    [Required(ErrorMessage = "Nombre Obligatorio")]
+    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Solo se permiten letras")]
     public string? Nombre { get; set; }
 
-    public int Matricula { get; set; }
+    [Required(ErrorMessage = "Matricula obligatoria")]
+    [StringLength(9, ErrorMessage = "La matricula solo debe tener 8 digitos")]
+    [RegularExpression(@"^\d{4}-\d{4}", ErrorMessage = "La matricula debe tener este formato xxxx-xxxx")]
+    public string? Matricula { get; set; }
 
+    [Required(ErrorMessage = "Carrera Obligatorio")]
+    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Solo se permiten letras")]
     public string? Carrera { get; set; }
 
 }
